3695764fe8cac2b09ae79b8a0511b4f0d6507c29
Loris Sadriu <loris.sadriu@sup-logistik.de>
2022-01-12 13:01:58 +0100

    Eingabe in die Datenbank

--- UniversalServer/Model/DBAccess.cs
+++ UniversalServer/Model/DBAccess.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Globalization;
 
 namespace UniversalServer.Model
 {
@@ -14,8 +16,32 @@ namespace UniversalServer.Model
        
         public void InsertData(TempValue t, HumidValue h, PressureValue p, DateTime dt, string ipa)
         {
-           
+            string twert = Convert.ToString(t.Value, CultureInfo.InvariantCulture);
+            string hwert = Convert.ToString(h.Value, CultureInfo.InvariantCulture);
+            string pwert = Convert.ToString(p.Value, CultureInfo.InvariantCulture);
+
+            string datum = dt.ToString("yyyy-M-dd" +
+                "H:mm:ss");
+
+
+            //Server muss noch angepasst werden
+            MySqlConnection con = new MySqlConnection(@"SERVER = 10.8.11.248;DATABASE=smarthome;UID=bui;PASSWORD=Smarthome123;");
+
+            con.Open();
             
+            //ohne IP/foreignkey Abfrage, da Lisa und Loris die meiste Zeit des Sprintes krank waren und am 12.1.22 alles C#-seitige und SQL-seitige erledigt wurde (2 Schulstunden)
+
+            string insert = "INSERT INTO smarthome.klimawerte (`temperatur`, `luftfeuchtigkeit`, `druck`, `zeitpunkt`, `ip`) VALUES('twert', 'hwert', 'pwert', 'datum', 'ipa')";
+
+            MySqlCommand cmd = new MySqlCommand();
+
+            cmd.CommandText = insert;
+
+            cmd.Connection = con;
+
+            cmd.ExecuteNonQuery();
+
+
         }
     }
 }
